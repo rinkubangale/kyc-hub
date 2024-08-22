63a0a94646b929cf55c10d8f9e10025ffcbd4fff
Rinku Bangale <bangalehemant@gmail.com>
2024-08-22 13:39:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import ProductDetails from "./ProductDetails";
+import CompareProducts from "./CompareProducts";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout style={{ height: "100vh" }}>
+        {/* Navbar */}
+        <Header style={{ background: "#fff", padding: 0 }}>
+          <Navbar />
+        </Header>
+
+        <Layout>
+          {/* Sidebar */}
+          <Sider width={200} style={{ background: "#fff" }}>
+            <Sidebar />
+          </Sider>
+
+          {/* Content Area */}
+          <Layout style={{ padding: "0 24px 24px" }}>
+            <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
+              <Routes>
+                <Route path="/" element={<ProductDetails />} />
+                <Route path="/compare" element={<CompareProducts />} />
+              </Routes>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
